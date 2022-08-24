5120466a573073bba9ec674e66546c3d1ade3d5f
jjenus <biz.jenus@gmail.com>
2022-08-24 01:15:12 +0000

    removed nextline 8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
     for i in range(len(str)):
         aski = ord(str[i])
         if aski <= 90:
-            print(str[i])
+            print(str[i], end='')
         else:
-            print(chr(aski-32))
+            print(chr(aski-32), end="")
