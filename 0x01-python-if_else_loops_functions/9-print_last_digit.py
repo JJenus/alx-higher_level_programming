97be0bd9ed32859f878c9617a2a4ce15b6171fc0
jjenus <biz.jenus@gmail.com>
2022-08-24 00:26:53 +0000

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if(number < 10):
+        print(number)
+        return number
+
+    if (number < 0):
+        number = number * -1
+    
+    number = number % 10
+    print(number)
+
+    return number
