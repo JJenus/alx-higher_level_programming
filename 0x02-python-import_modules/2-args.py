8d77f7e0306ee35093a7b4c4cda5b105a92e45ea
jjenus <biz.jenus@gmail.com>
2022-08-28 21:03:06 +0000

    added 4-hidden_discovery.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     plural = 's' if len(argv)-1 != 1 else ''
     end = ':' if len(argv)-1 > 0 else '.'
-    print(f"{len(argv)-1} argument{plural+end}")
+    print("{} argument{}".format(len(argv)-1, plural+end))
     for i in range(1, len(argv)):
-        print(f"{i}. {argv[i]}")
+        print("{}: {}".format(i, argv[i]))
