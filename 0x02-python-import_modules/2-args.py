c3f2d9ceb3012f62d500bc032004829f382daa0d
jjenus <biz.jenus@gmail.com>
2022-08-28 20:22:49 +0000

    updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print(f"{len(argv)-1} arguments")
+    plural = 's' if len(argv)-1 != 1 else ''
+    end = ':' if len(argv)-1 > 0 else '.'
+    print(f"{len(argv)-1} argument{plural+end}")
     for i in range(1, len(argv)):
         print(f"{i}. {argv[i]}")
