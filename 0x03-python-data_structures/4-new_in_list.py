336440c351659a2c2f3bc5d511e418ddaf3a3fbd
jjenus <biz.jenus@gmail.com>
2022-08-29 14:05:37 +0000

    added 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    nlist = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return nlist
+    nlist[idx] = element
+    return nlist
