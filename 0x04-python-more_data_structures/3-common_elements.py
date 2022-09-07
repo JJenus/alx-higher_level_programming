201b4e9bd694c281e53503eb50026afec2622445
jjenus <biz.jenus@gmail.com>
2022-09-07 20:25:09 +0000

    updated 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    return [s for s in set([i for i in set_1 for j in set_2 if i == j])]
-
-print(common_elements({'java', 'gr', 'c'}, {'iu', 'c', 'op', 'c'}))
+    set_3 = [i for i in set(set_1) if i in set_2]
+    return list(set(set_3 + [i for i in set(set_2) if i in set_1]))
