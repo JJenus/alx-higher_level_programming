3d3933415c591e2637eb6923c26862935b3dfd31
jjenus <biz.jenus@gmail.com>
2022-09-09 16:16:42 +0000

    10-best_score.py update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
+    if len(a_dictionary < 1):
+        return None
     score = -9999
     for k, i in a_dictionary.items():
         if i > score:
