e664a484c7c55dee7dc79b45623dd5141d140d56
jjenus <biz.jenus@gmail.com>
2022-09-09 16:49:29 +0000

    returned key instead of val

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if len(a_dictionary) < 1:
-        return None
     score = -9999
+    key = None
     for k, i in a_dictionary.items():
         if i > score:
             score = i
-    return score
+            key = k
+    return key
