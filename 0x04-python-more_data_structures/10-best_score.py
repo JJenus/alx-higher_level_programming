7e533231b37df5cd3a04ab4df67f3dc08de51e72
jjenus <biz.jenus@gmail.com>
2022-09-09 16:46:35 +0000

    corrected checker 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if len(a_dictionary < 1):
+    if len(a_dictionary) < 1:
         return None
     score = -9999
     for k, i in a_dictionary.items():
