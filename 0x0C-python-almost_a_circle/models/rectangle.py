62b69eddbc6e47108ad1d48d654f8695131f3849
jjenus <biz.jenus@gmail.com>
2022-10-03 21:28:15 +0000

    added rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+'''
+    Class Rectangle
+'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("y", value)
+        self.__y = value
+
+    def area(self):
+        '''
+            Returns the area of the rectangle
+        '''
+        return (self.height * self.width)
+    
+    @staticmethod
+    def setter_validation(attribute, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+            elif value <= 0:
+                raise ValueError("{} must be > 0".format(attribute))
+
