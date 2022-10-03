5eb875756a7a40bcd2fcc42f369feb5b3bba4e2a
jjenus <biz.jenus@gmail.com>
2022-10-03 17:44:11 +0000

    added 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Read file module """
+
+
+def read_file(filename=""):
+    """read file
+    Args:
+        filename (str): location/path
+    """
+    with open(filename, "w", encoding="UTF-8") as f:
+        print(f.read())
